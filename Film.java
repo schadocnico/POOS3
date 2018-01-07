b79dea893506e3b5a3b54faaa30136fd12aaf1ab
Nicolas <schadocnico@gmail.com>
2018-01-07 16:38:52 +0100

    Add files via upload

--- Film.java
+++ Film.java
@@ -5,4 +5,11 @@ import java.util.Date;
 public class Film extends Spectacle{
 	String realisateur;
 	Date duree;
+	
+	public Film(String titre, String interpretes, String realisateur, Date duree) {
+		super(titre, interpretes);
+		this.realisateur = realisateur;
+		this.duree = duree;
+	}
+	
 }
--- Spectacle.java
+++ Spectacle.java
@@ -2,4 +2,10 @@ package projetS3;
 
 public abstract class Spectacle {
 	private String titre, interpretes;
+	
+	public Spectacle(String titre, String interpretes) {
+		super();
+		this.titre = titre;
+		this.interpretes = interpretes;
+	}
 }
